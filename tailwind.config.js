3ea25a2af3dce8c8af31c3aaa3e15846875a6d77
Camilo Cuitiva <camiloandres174@gmail.com>
2022-08-20 17:30:13 -0500

    Se grega el navbar y el hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      primary: '#f9f8f3',
+      secondary: '#4e13d1',
+    },
   },
   plugins: [require('flowbite/plugin')],
 }
